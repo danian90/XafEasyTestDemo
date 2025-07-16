919cb960057cd1b33f901d2ff9c35a27e575a07d
Danian Isaac León <danian.isaac90@gmail.com>
2025-07-16 12:40:47 -0400

    Agregar archivos de proyecto.

---
+++ XafEasyTestDemo.Module/BusinessObjects/Student.cs
@@ -0,0 +1,59 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.DC;
+using DevExpress.ExpressApp.Model;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.Validation;
+using DevExpress.Xpo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace XafEasyTestDemo.Module.BusinessObjects
+{
+    [DefaultClassOptions]
+    //[ImageName("BO_Contact")]
+    //[DefaultProperty("DisplayMemberNameForLookupEditorsOfThisType")]
+    //[DefaultListViewOptions(MasterDetailMode.ListViewOnly, false, NewItemRowPosition.None)]
+    //[Persistent("DatabaseTableName")]
+    // Specify more UI options using a declarative approach (https://docs.devexpress.com/eXpressAppFramework/112701/business-model-design-orm/data-annotations-in-data-model).
+    public class Student : BaseObject
+    { // Inherit from a different class to provide a custom primary key, concurrency and deletion behavior, etc. (https://docs.devexpress.com/eXpressAppFramework/113146/business-model-design-orm/business-model-design-with-xpo/base-persistent-classes).
+        // Use CodeRush to create XPO classes and properties with a few keystrokes.
+        // https://docs.devexpress.com/CodeRushForRoslyn/118557
+        public Student(Session session)
+            : base(session)
+        {
+        }
+        public override void AfterConstruction()
+        {
+            base.AfterConstruction();
+        }
+
+        string lastName;
+        string firstName;
+
+        [Size(SizeAttribute.DefaultStringMappingFieldSize)]
+        public string FirstName
+        {
+            get => firstName;
+            set => SetPropertyValue(nameof(FirstName), ref firstName, value);
+        }
+
+        [Size(SizeAttribute.DefaultStringMappingFieldSize)]
+        public string LastName
+        {
+            get => lastName;
+            set => SetPropertyValue(nameof(LastName), ref lastName, value);
+        }
+        
+        [Size(SizeAttribute.DefaultStringMappingFieldSize)]
+        public string FullName
+        {
+            get => FirstName + " "+ LastName;
+        }
+    }
+}
---
+++ XafEasyTestDemo.Module/BusinessObjects/Task.cs
@@ -0,0 +1,42 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.DC;
+using DevExpress.ExpressApp.Model;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.Validation;
+using DevExpress.Xpo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace XafEasyTestDemo.Module.BusinessObjects
+{
+    [DefaultClassOptions]
+    public class Task : BaseObject
+    { 
+        public Task(Session session)
+            : base(session)
+        {
+        }
+        public override void AfterConstruction()
+        {
+            base.AfterConstruction();
+        }
+        private string description;
+        public string Description
+        {
+            get => description;
+            set => SetPropertyValue(nameof(Description), ref description, value);
+        }
+
+        private DateTime dueDate;
+        public DateTime DueDate
+        {
+            get => dueDate;
+            set => SetPropertyValue(nameof(DueDate), ref dueDate, value);
+        }
+    }
+}
